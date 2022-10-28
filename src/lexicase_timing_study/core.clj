d72d0eb48812c243bcec38754eff37b426a7ad00
Nic McPhee <mcphee@morris.umn.edu>
2022-10-28 14:47:30 -0500

    Implement lexicase with ArrayList
    This uses `ArrayList` to implement lexicase selection in a way that mirrors the best Rust implementation so far.
    
    I also ran some benchmarking with this, and that's in the new timing results output file.

--- src/lexicase_timing_study/core.clj
+++ src/lexicase_timing_study/core.clj
@@ -76,6 +76,50 @@
         (rest survivors))
        (rest cases)))))
 
+(defn lexicase-array-list
+  [pop]
+  (let [num-individuals (count pop)]
+    ; `candidates` is the list of survivors through the lexicase process so far.
+    ; `winners` is the list of individuals that have survived the on the current
+    ; test case.
+    (loop [candidates (java.util.ArrayList. pop)
+           winners (java.util.ArrayList. num-individuals)
+           cases (shuffle (range (count (:errors (first pop)))))]
+      (if (or (empty? cases)
+              (empty? (rest candidates)))
+        ; We need to convert the ArrayList back to a vec so we can
+        ; call rand-nth on it. Alternatively we could just pick a
+        ; random instance and call `.get` with that; this might be
+        ; more efficient since it would avoid that conversion.
+        (rand-nth (into [] candidates))
+        (do
+          (.clear winners)
+          ; We'll put the first candidate in the winners pool
+          ; straight away as "the best we've seen so far"
+          (.add winners (.get candidates 0))
+          (recur
+           (do
+             (doseq
+              ; We've already "processed" the first candidate by
+              ; adding it straight away to the winners, so we'll
+              ; only loop over 1..(num-individuals - 1).
+              ; pop-indices (range num-individuals)
+              [i (range 1 (count candidates))]
+               (let [best-error (nth (:errors (.get winners 0)) (first cases))
+                     this-error (nth (:errors (.get candidates i)) (first cases))]
+                 (when (< this-error best-error)
+                   ; This is better than anything we've seen so far so
+                   ; clear winners and add this.
+                   (.clear winners)
+                   (.add winners (.get candidates i)))
+                 (when (= this-error best-error)
+                   ; This is the same as the best we've seen so far, so
+                   ; add it to the winners.
+                   (.add winners (.get candidates i)))))
+             winners)
+           candidates
+           (rest cases)))))))
+
 (defn check-pop
   [pop pop-size len]
   (assert (= (count pop) pop-size))
@@ -103,11 +147,13 @@
      [(make-num-distinct-pop 1000 500 100 20) "500_random_values_population" 500]
      [(make-num-distinct-pop 1000 1000 100 20) "1000_random_values_population" 1000]]
     [selector selector-descriptor]
-    [[lexicase-selection "lexicase"]
+    [[lexicase-array-list "lexicase array list"]
+     ; [lexicase-selection "lexicase"]
      [lexicase-selection-no-grouping "lexicase w/o grouping"]
-     [lexicase-single-pass "lexicase single pass"]]]
+     ; [lexicase-single-pass "lexicase single pass"]
+     ]]
     (check-pop pop 1000 100)
     (println (str selector-descriptor "\t" pop-descriptor "\t" num-values))
     (bench/bench
-     (selector pop)
+     (frequencies (repeatedly 1000 #(selector pop)))
      :verbose)))
